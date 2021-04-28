e2ae3e991395c57c1bb78b43d4ff90f702fae8bb
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-04-28 19:02:46 -0400

    including included Tuple in print

--- singleSensorTest.py
+++ singleSensorTest.py
@@ -23,8 +23,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
 
 #Attempt to print sensor object properties:
-for property, value in vars(sensor).items():
-    print(property, ":", value)
+#for property, value in vars(sensor).items():
+#    print(property, ":", value)
 
 
 # Main loop reading color and printing it every second.
@@ -32,6 +32,7 @@ while True:
     # Read the color temperature and lux of the sensor too.
     temp = sensor.color_temperature
     lux = sensor.lux
-    print("Temperature: {0}K Lux: {1}".format(temp, lux))
+    colorTuple = sensor.color_rgb_bytes 
+    print("Temperature: {0}\tK Lux: {1}\tcolorTuple: {2}".format(temp, lux, colorTuple))
     # Delay for a second and repeat.
     time.sleep(1.0)
