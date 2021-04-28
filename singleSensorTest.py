792639554a59af35e6593a6b75d5b9fef1186626
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-04-28 18:53:30 -0400

    Added print output for temp and lux

--- singleSensorTest.py
+++ singleSensorTest.py
@@ -7,7 +7,7 @@ import adafruit_character_lcd.character_lcd as character_lcd
 
 #intializeLCD
 lcd_columns = 16
-lcd_rows =2 
+lcd_rows =2
 
 lcd_rs = digitalio.DigitalInOut(board.D26)
 lcd_en = digitalio.DigitalInOut(board.D19)
@@ -21,3 +21,12 @@ lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,lc
 #initializeColorSensor
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
+
+# Main loop reading color and printing it every second.
+while True:
+    # Read the color temperature and lux of the sensor too.
+    temp = sensor.color_temperature
+    lux = sensor.lux
+    print("Temperature: {0}K Lux: {1}".format(temp, lux))
+    # Delay for a second and repeat.
+    time.sleep(1.0)
