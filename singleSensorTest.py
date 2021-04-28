fc06483660bf121c5b68fbd11e23c91d6ae13a81
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-04-28 18:58:11 -0400

    Attempting to print sensor object properties

--- singleSensorTest.py
+++ singleSensorTest.py
@@ -22,6 +22,11 @@ lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,lc
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
 
+#Attempt to print sensor object properties:
+for property, value in vars(sensor).items():
+    print(property, ":", value)
+
+
 # Main loop reading color and printing it every second.
 while True:
     # Read the color temperature and lux of the sensor too.
