ddc16e7f8f3f3b652273b897ac31a4d5e77b6f9d
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-04-28 19:55:47 -0400

    all four sensors

--- singleSensorTest.py
+++ singleSensorTest.py
@@ -23,11 +23,11 @@ lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,lc
 i2c = busio.I2C(board.SCL, board.SDA)
 #sensor = adafruit_tcs34725.TCS34725(i2c)
 tca = adafruit_tca9548a.TCA9548A(i2c)
-sensors = [0,0]
+sensors = [0,0,0,0]
 sensors[0] = adafruit_tcs34725.TCS34725(tca[0])
 sensors[1] = adafruit_tcs34725.TCS34725(tca[1])
-#sensor2 = adafruit_tcs34725.TCS34725(tca[2])
-#sensor3 = adafruit_tcs34725.TCS34725(tca[3])
+sensors[2] = adafruit_tcs34725.TCS34725(tca[2])
+sensors[3] = adafruit_tcs34725.TCS34725(tca[3])
 
 #Attempt to print sensor object properties:
 #for property, value in vars(sensor).items():
@@ -43,7 +43,6 @@ while True:
     lux = int(sensor.lux)
     colorTuple = sensor.color_rgb_bytes 
     str0=("s0:\tTmp: {0}\tLux: {1}".format(temp, lux))
-    # Delay for a second and repeat.
     
     
     sensor = sensors[1]
@@ -52,6 +51,22 @@ while True:
     lux = int(sensor.lux)
     colorTuple = sensor.color_rgb_bytes 
     str1=("\t\ts1:\tTmp: {0}\tK Lux: {1}".format(temp, lux))
+    
+    sensor = sensors[2]
+    # Read the color temperature and lux of the sensor too.
+    temp = int(sensor.color_temperature)
+    lux = int(sensor.lux)
+    colorTuple = sensor.color_rgb_bytes 
+    str2=("\t\ts2:\tTmp: {0}\tK Lux: {1}".format(temp, lux))
+    
+    sensor = sensors[3]
+    # Read the color temperature and lux of the sensor too.
+    temp = int(sensor.color_temperature)
+    lux = int(sensor.lux)
+    colorTuple = sensor.color_rgb_bytes 
+    str3=("\t\ts3:\tTmp: {0}\tK Lux: {1}".format(temp, lux))
     # Delay for a second and repeat.
-    print(str0+str1)
+    
+    
+    print(str0+str1+str2+str3)
     time.sleep(0.25)
