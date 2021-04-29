ea1604a8dcaa1fa210202e92b1bbab9a0fb08213
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-04-28 20:47:47 -0400

    cleanup, probably will cause an error

--- unifiedTest.py
+++ unifiedTest.py
@@ -5,6 +5,17 @@ import adafruit_tca9548a
 import busio
 import digitalio
 import adafruit_character_lcd.character_lcd as character_lcd
+import sys
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+
+gpio_channels = [24,26,35,37]       #Channel number will be output pin number
+
+
+GPIO.setmode(GPIO.BOARD)            #Sets mode to board numbering
+
+GPIO.setup(gpio_channels, GPIO.OUT, initial=GPIO.LOW) # sets up channels (GPIO pins) as an outputs for the motors
+
 
 #intializeLCD
 lcd_columns = 16
@@ -19,12 +30,13 @@ lcd_d4 = digitalio.DigitalInOut(board.D13)
 
 lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,lcd_d7, lcd_columns, lcd_rows)
 
-#initializeColorSensor
+#initialize i2c 
 i2c = busio.I2C(board.SCL, board.SDA)
-#sensor = adafruit_tcs34725.TCS34725(i2c)
+#initialize multiplexor
 tca = adafruit_tca9548a.TCA9548A(i2c)
+#create sensor array
 sensors = [0,0,0,0]
-sensorLux = [-1,-1,-1,-1]
+sensorsLux = [-1,-1,-1,-1]
 sensors[0] = adafruit_tcs34725.TCS34725(tca[0])
 sensors[1] = adafruit_tcs34725.TCS34725(tca[1])
 sensors[2] = adafruit_tcs34725.TCS34725(tca[2])
@@ -34,15 +46,23 @@ sensors[3] = adafruit_tcs34725.TCS34725(tca[3])
 
 
 
-# Main loop reading color and printing it every second.
-while True:
-    
-    str = ""
-    n=0
-    for sensor in sensors:
-        lux = int(sensor.lux)
-        str+=("s{0} Lux: {1}".format(n,lux))
-        n++
-    
-    print(str)
-    time.sleep(0.25)
+
+
+
+def moveForward(n):
+    GPIO.output(24, GPIO.HIGH)
+    GPIO.output(35, GPIO.HIGH)
+    GPIO.output(26, GPIO.LOW)
+    GPIO.output(37, GPIO.LOW)
+    time.sleep(n)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.output(35, GPIO.LOW)
+
+def moveBackward(n):
+    GPIO.output(26, GPIO.HIGH)
+    GPIO.output(37, GPIO.HIGH)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.output(35, GPIO.LOW)
+    time.sleep(n)
+    GPIO.output(26, GPIO.LOW)
+    GPIO.output(37, GPIO.LOW)
