d213f5cb7d79dd0c03c2f9d25f0b56c0dc804cbe
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-05-06 19:46:13 -0400

    disable running on startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 cd /home/pi/Documents/pi-napple
 git pull >> /home/pi/Desktop/stdout.txt 2>> /home/pi/Desktop/stderr.txt
-python3 /home/pi/Documents/pi-napple/channelTestv2.py >> /home/pi/Desktop/stdout.txt 2>> /home/pi/Desktop/stderr.txt
+#python3 /home/pi/Documents/pi-napple/channelTestv2.py >> /home/pi/Desktop/stdout.txt 2>> /home/pi/Desktop/stderr.txt
