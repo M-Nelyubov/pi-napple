0f0a0dd757f62b1f31c1d45997464d1e062da9dc
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-05-06 19:34:13 -0400

    STDOUT added

--- unifiedTest.py
+++ unifiedTest.py
@@ -9,11 +9,13 @@ import sys
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 
+print("Initializing GPIO channels...")
 gpio_channels = [24,26,35,37]       #Channel number will be output pin number
 
-
+print("setting GPIO mode to BOARD")
 GPIO.setmode(GPIO.BOARD)            #Sets mode to board numbering
 
+print("setting up GPIO channels as outputs")
 GPIO.setup(gpio_channels, GPIO.OUT, initial=GPIO.LOW) # sets up channels (GPIO pins) as an outputs for the motors
 
 
@@ -21,6 +23,7 @@ GPIO.setup(gpio_channels, GPIO.OUT, initial=GPIO.LOW) # sets up channels (GPIO p
 lcd_columns = 16
 lcd_rows =2
 
+print("Initializing LCD pins")
 lcd_rs = digitalio.DigitalInOut(board.D26)
 lcd_en = digitalio.DigitalInOut(board.D19)
 lcd_d7 = digitalio.DigitalInOut(board.D21)
@@ -28,13 +31,17 @@ lcd_d6 = digitalio.DigitalInOut(board.D5)
 lcd_d5 = digitalio.DigitalInOut(board.D6)
 lcd_d4 = digitalio.DigitalInOut(board.D13)
 
+print("creating an instance of character_lcd")
 lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,lcd_d7, lcd_columns, lcd_rows)
 
+print("initializing i2c")
 #initialize i2c 
 i2c = busio.I2C(board.SCL, board.SDA)
 #initialize multiplexor
+print("initializing adafruit Mux")
 tca = adafruit_tca9548a.TCA9548A(i2c)
 #create sensor array
+print("creating sensor array")
 sensors = [0,0,0,0]
 sensorsLux = [-1,-1,-1,-1]
 sensors[0] = adafruit_tcs34725.TCS34725(tca[0]) #back right
@@ -42,7 +49,10 @@ sensors[1] = adafruit_tcs34725.TCS34725(tca[1]) #back left
 sensors[2] = adafruit_tcs34725.TCS34725(tca[2]) #front left
 sensors[3] = adafruit_tcs34725.TCS34725(tca[3]) #front right
 
+
+
 def refreshSensorLux():
+    print("refreshing sensors")
     str = ""
     n=0
     for i in range(4):
@@ -102,7 +112,7 @@ def turnLeft(n):
 
 
 
-
+print("beginning test loop")
 refreshSensorLux()
 while(sensors[0] and sensors[1]):
     turnLeft(0.01)
