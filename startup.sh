54dc354a893f3c1e6951925077dd8e404bc5c2f7
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-05-06 22:41:48 -0400

    pull at end

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 cd /home/pi/Documents/pi-napple
 git pull >> /home/pi/Desktop/stdout.txt 2>> /home/pi/Desktop/stderr.txt
 python3 /home/pi/Documents/pi-napple/reunifiedTest.py >> /home/pi/Desktop/stdout.txt 2>> /home/pi/Desktop/stderr.txt
+git pull >> /home/pi/Desktop/stdout.txt 2>> /home/pi/Desktop/stderr.txt
