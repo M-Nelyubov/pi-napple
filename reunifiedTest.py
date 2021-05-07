9c9be993785eea7a74cc5d3b9b2d5b8ebc1be756
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-05-06 20:17:17 -0400

    moved sensor segment to run second

--- reunifiedTest.py
+++ reunifiedTest.py
@@ -1,42 +1,3 @@
-import time
-import board
-import adafruit_tcs34725
-import adafruit_tca9548a
-import busio
-
-
-#initializeColorSensor
-i2c = busio.I2C(board.SCL, board.SDA)
-#sensor = adafruit_tcs34725.TCS34725(i2c)
-tca = adafruit_tca9548a.TCA9548A(i2c)
-sensors = [0,0,0,0]
-sensorLux = [-1,-1,-1,-1]
-sensors[0] = adafruit_tcs34725.TCS34725(tca[0])
-sensors[1] = adafruit_tcs34725.TCS34725(tca[1])
-sensors[2] = adafruit_tcs34725.TCS34725(tca[2])
-sensors[3] = adafruit_tcs34725.TCS34725(tca[3])
-
-
-
-# Main loop reading color and printing it every second.
-def refreshSensorLux():
-    str = ""
-    n=0
-    for sensor in sensors:
-        lux = int(sensor.lux)
-        str+=("s{0} Lux: {1}\t".format(n,lux))
-        n= n+1
-    
-    print(str)
-    
-    
-    
-    
-refreshSensorLux()
-
-print("Completed sensor initialization")
-
-
 print("Initializing motors")
 
 
@@ -98,3 +59,46 @@ GPIO.setmode(GPIO.BOARD)            #Sets mode to board numbering
 ###moveForward(0.5)
 ###print("Moving backward")
 ###moveBackward(0.5)
+
+
+
+
+import time
+import board
+import adafruit_tcs34725
+import adafruit_tca9548a
+import busio
+
+
+#initializeColorSensor
+i2c = busio.I2C(board.SCL, board.SDA)
+#sensor = adafruit_tcs34725.TCS34725(i2c)
+tca = adafruit_tca9548a.TCA9548A(i2c)
+sensors = [0,0,0,0]
+sensorLux = [-1,-1,-1,-1]
+sensors[0] = adafruit_tcs34725.TCS34725(tca[0])
+sensors[1] = adafruit_tcs34725.TCS34725(tca[1])
+sensors[2] = adafruit_tcs34725.TCS34725(tca[2])
+sensors[3] = adafruit_tcs34725.TCS34725(tca[3])
+
+
+
+# Main loop reading color and printing it every second.
+def refreshSensorLux():
+    str = ""
+    n=0
+    for sensor in sensors:
+        lux = int(sensor.lux)
+        str+=("s{0} Lux: {1}\t".format(n,lux))
+        n= n+1
+    
+    print(str)
+    
+    
+    
+    
+refreshSensorLux()
+
+print("Completed sensor initialization")
+
+
